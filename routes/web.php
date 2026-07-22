f8f5c743105790dc66d42ff94aa86bad59be8660
kreativesahara <technologieslensmen@gmail.com>
2026-07-22 17:25:36 +0300

    feat: add Apache configuration for CORS and helper route to clear application cache

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/clear-cache', function() {
+    \Illuminate\Support\Facades\Artisan::call('config:clear');
+    \Illuminate\Support\Facades\Artisan::call('cache:clear');
+    \Illuminate\Support\Facades\Artisan::call('route:clear');
+    \Illuminate\Support\Facades\Artisan::call('view:clear');
+    return 'All caches cleared successfully!';
+});
